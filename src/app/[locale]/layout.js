7dd4640768ccdd3fb7a22c55943651ec56911f12
0xdeeznode <nalanarud@gmail.com>
2024-08-24 06:32:58 +0400

    finish first slide of about

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -7,7 +7,7 @@ import Footer from "@/components/footer/Footer";
 import Contact from "@/components/contact/Contact";
 
 export const metadata = {
-  title: "Dubai Real Estate | Buy, Sell, Rent & Invest in Properties",
+  title: "Dubai Real Estate | Buy, Sell & Invest in Properties",
   description: "Expert real estate services in Dubai. Buy, sell, rent, and invest in properties with confidence. Schedule a property consultation with our experienced agents today!",
   keywords: "Dubai real estate, buy property Dubai, sell property Dubai, rent property Dubai, invest in Dubai, property consultation Dubai",
   author: "AlDuran Real Estate",
--- src/components/about/Why.jsx
+++ src/components/about/Why.jsx
@@ -5,14 +5,25 @@ import Image from 'next/image'
 const Why = () => {
   const t = useTranslations('Why');
   return (
-    <div className='flex flex-col w-full h-screen justify-evenly items-center p-4 md:px-10'>
-      <header className='w-full text-center text-white font-bona text-3xl'><h3>{t('title')}</h3></header>
-      <div className='flex'>
-        <div className='flex flex-col w-[45%] gap-2 text-white'>
-          <h3 className='text-2xl font-bona'></h3>
-          <p className='font-titillium'>{t('body')}</p>
+    <section className='flex flex-col w-full h-screen justify-center items-center p-2 md:px-6'>
+      {/* First Card */}
+      <div className='flex w-full justify-between items-center'>
+        <div className='flex flex-col w-[46%] gap-4 md:gap-8'>
+          <div className='flex flex-col md:items-center gap-1 glass-effect p-2 text-white'>
+            <h3 className='text-xl md:text-3xl font-bona'>{t('title-1')}</h3>
+            <p className='font-titillium text-md md:text-xl'>{t('body-1')}</p>
+          </div>
+          <div className='flex flex-col md:items-center gap-1 glass-effect p-2 text-white'>
+            <h3 className='text-xl md:text-3xl font-bona'>{t('title-2')}</h3>
+            <p className='font-titillium text-md md:text-xl'>{t('body-2')}</p>
+          </div>
+          <div className='flex flex-col md:items-center gap-1 glass-effect p-2 text-white'>
+            <h3 className='text-xl md:text-3xl font-bona'>{t('title-3')}</h3>
+            <p className='font-titillium text-md md:text-xl'>{t('body-3')}</p>
+          </div>
         </div>
-        <div>
+        <div className='flex flex-col items-center gap-12 w-[50%] md:w-[45%]'>
+          <header className='w-full text-center text-white font-bona text-[40px] leading-tight'><h3>{t('header')}</h3></header>
           <Image
           src='/desktop-why.jpg'
           alt='Why Invest in Dubai'
@@ -29,7 +40,7 @@ const Why = () => {
           />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
